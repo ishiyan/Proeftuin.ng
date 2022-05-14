64fe597e43ddd614b240e3ee595c86a1527ec129
ivan shiyan <ivan.shiyan@gmail.com>
2022-05-14 17:10:31 +0200

    updated versions and fixed ex06

---
+++ build_prod_all.sh
@@ -0,0 +1,47 @@
+echo ///////////////////////////////////////////////////////////
+echo  ex01a: A responsive navbar using a Flex Layout: part 1
+./node_modules/.bin/ng build ex01a
+
+echo ///////////////////////////////////////////////////////////
+echo ex01b: A responsive navbar using a Flex Layout: part 2
+./node_modules/.bin/ng build ex01b
+
+echo ///////////////////////////////////////////////////////////
+echo ex02: A responsive sidebar menu with Angular Material
+./node_modules/.bin/ng build ex02
+
+echo ///////////////////////////////////////////////////////////
+echo ex03: A responsive card grid using a Flex Layout
+./node_modules/.bin/ng build ex03
+
+echo ///////////////////////////////////////////////////////////
+echo ex03b: Fastest way to make a responsive card grid with CSS
+./node_modules/.bin/ng build ex03b
+
+echo ///////////////////////////////////////////////////////////
+echo ex04: A responsive pure CSS masonry layout
+./node_modules/.bin/ng build ex04
+
+echo ///////////////////////////////////////////////////////////
+echo ex05a: A multi-select chips component based on Angular Material
+./node_modules/.bin/ng build ex05a
+
+echo ///////////////////////////////////////////////////////////
+echo ex05b: Angular Material multi-select chips
+./node_modules/.bin/ng build ex05b
+
+echo ///////////////////////////////////////////////////////////
+echo ex06: Angular Material Dark Mode
+./node_modules/.bin/ng build ex06
+
+echo ///////////////////////////////////////////////////////////
+echo ex07: Truncate multiple line text in Angular
+./node_modules/.bin/ng build ex07
+
+echo ///////////////////////////////////////////////////////////
+echo ex08: Reusable Confirmation Dialogs with Angular Material
+./node_modules/.bin/ng build ex08
+
+echo ///////////////////////////////////////////////////////////
+echo ex09: Create Dynamic Form Controls in Angular with FormArray
+./node_modules/.bin/ng build ex09
